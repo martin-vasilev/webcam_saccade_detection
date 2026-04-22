edb998a0fd5f34faf6b72b3c3b0aca724f904d1f
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2026-04-22 09:16:33 +0100

    processed third subject

--- data/manual_labels/prep_manual_labels.R
+++ data/manual_labels/prep_manual_labels.R
@@ -1,11 +1,17 @@
 
+rm(list= ls())
+
 library(readr)
 sub11 <- read_csv("data/manual_labels/sub11.csv")
 sub11$subject<-11
+
 sub13 <- read_csv("data/manual_labels/sub13.csv")
 sub13$subject<-13
 
-dat<- rbind(sub11, sub13)
+sub14 <- read_csv("data/manual_labels/sub14.csv")
+sub14$subject<-14
+
+dat<- rbind(sub11, sub13, sub14)
 
 # need to map blinks to labels
 
