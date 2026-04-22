83973f98bd4d8ee3cf90fdd2f6165edb19199207
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2026-04-22 11:58:18 +0100

    Add manual labels to webcam data

--- data/manual_labels/prep_manual_labels.R
+++ data/manual_labels/prep_manual_labels.R
@@ -1,19 +1,49 @@
 
 rm(list= ls())
 
+# subject numbers to process:
+
+subs<- c(11,13,14)
+dat<- NULL
 library(readr)
-sub11 <- read_csv("data/manual_labels/sub11.csv")
-sub11$subject<-11
 
-sub13 <- read_csv("data/manual_labels/sub13.csv")
-sub13$subject<-13
-
-sub14 <- read_csv("data/manual_labels/sub14.csv")
-sub14$subject<-14
-
-dat<- rbind(sub11, sub13, sub14)
-
-# need to map blinks to labels
+for(i in 1:length(subs)){
+  sub<- read_csv(paste("data/manual_labels/sub",
+                       toString(subs[i]), '.csv', sep= ''))
+  sub$subject<- subs[i]
+  dat<- rbind(dat, sub)
+}
 
 write.csv(dat,'data/manual_ground_truth.csv')
+
+## map manual ground truth to sample data:
+
+web <- read_csv('data/webcam_data_ground_truth.csv')
+
+# subset data to subjects we have manual labels for:
+web<- subset(web, is.element(sub, subs) & Task_Name== 'Single_line_sentences')
+web$ground_truth<- NA
+web$before_1stfix<- NULL
+
+# map labels to data:
+
+for(i in 1:nrow(web)){
+  # take only events within this trial:
+  trial<- dat[which(dat$subject== web$sub[i] & dat$trial== web$Trial_Id[i]-100),]
+  
+  # find which event belongs to sample:
+  evnt<- which(trial$start_time<=web$el_time[i] & trial$end_time>= web$el_time[i])
+  if(length(evnt)>0){
+    web$ground_truth[i]<- trial$label[evnt]
+  }
+}
+
+# need to map blinks to labels:
+web$ground_truth[which(web$el_pupil==0)]<- "blink"
+
+# save raw sample data with manual labels:
+write.csv(web, 'data/manual_labels/webdata_manual_labels.csv')
+
+
+
 
